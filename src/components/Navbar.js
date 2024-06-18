7620fc372e0b844e49781c03e7b15b29dcf5f397
Abinashvirat <abhinashpradhan66018@gmail.com>
2024-06-18 23:07:45 +0530

    up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Navbar({ scrollToSection, name }) {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <header>
       <nav>
         <div className="left">
-          {/* Abinash's Portfolio */}
           {name}
         </div>
-        <button className="navbar-toggler" >
+        <button className="navbar-toggler" onClick={handleToggle}>
           <i className="fas fa-bars"></i>
         </button>
-        <div className="right">
+        <div className={`right ${isNavOpen ? 'active' : ''}`}>
           <ul>
-            <li><a href="#home" onClick={() => scrollToSection('home')}>Home</a></li>
-            <li><a href="#about" onClick={() => scrollToSection('about')}>About</a></li>
-            <li><a href="#skills" onClick={() => scrollToSection('skills')}>Skills</a></li>
-            <li><a href="#projects" onClick={() => scrollToSection('projects')}>Projects</a></li>
+            <li><a href="#home" onClick={() => { scrollToSection('home'); handleToggle(); }}>Home</a></li>
+            <li><a href="#about" onClick={() => { scrollToSection('about'); handleToggle(); }}>About</a></li>
+            <li><a href="#skills" onClick={() => { scrollToSection('skills'); handleToggle(); }}>Skills</a></li>
+            <li><a href="#projects" onClick={() => { scrollToSection('projects'); handleToggle(); }}>Projects</a></li>
           </ul>
         </div>
       </nav>
     </header>
   );
 }
+
--- src/components/Section4.js
+++ src/components/Section4.js
@@ -1,15 +1,18 @@
 import React from 'react';
-
+import img1 from '../img/1.jpg';
+import img2 from '../img/2.jpg';
+import img3 from '../img/3.jpg';
+import img4 from '../img/4.jpg';
 
 export default function ProjectsSection() {
   return (
     <section className="fourthsection" id="Projects">
-        <br></br>
+      <br />
       <h2>Projects</h2>
       <br />
       <div className="card-container">
         <div className="card">
-          <img src="1.jpg" className="card-img-top" alt="Project 1" />
+          <img src={img1} className="card-img-top" alt="Project 1" />
           <div className="card-body">
             <p className="card-text">
               Some quick example text to build on the card title and make up the bulk of the card's content.
@@ -19,7 +22,7 @@ export default function ProjectsSection() {
           </div>
         </div>
         <div className="card">
-          <img src="2.jpg" className="card-img-top" alt="Project 2" />
+          <img src={img2} className="card-img-top" alt="Project 2" />
           <div className="card-body">
             <p className="card-text">
               Some quick example text to build on the card title and make up the bulk of the card's content.
@@ -29,7 +32,7 @@ export default function ProjectsSection() {
           </div>
         </div>
         <div className="card">
-          <img src="3.jpg" className="card-img-top" alt="Project 3" />
+          <img src={img3} className="card-img-top" alt="Project 3" />
           <div className="card-body">
             <p className="card-text">
               Some quick example text to build on the card title and make up the bulk of the card's content.
@@ -39,7 +42,7 @@ export default function ProjectsSection() {
           </div>
         </div>
         <div className="card">
-          <img src="4.jpg" className="card-img-top" alt="Project 4" />
+          <img src={img4} className="card-img-top" alt="Project 4" />
           <div className="card-body">
             <p className="card-text">
               Some quick example text to build on the card title and make up the bulk of the card's content.
